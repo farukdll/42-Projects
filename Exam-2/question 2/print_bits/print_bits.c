6e35ccb08ab207c0fbc2ad5b6b4db66f871c0bd7
Faruk PEKTAS <97880185+farukdll@users.noreply.github.com>
2023-10-14 20:10:37 +0300

    Exam-2/ added

--- Exam-2/question 2/print_bits/print_bits.c
+++ Exam-2/question 2/print_bits/print_bits.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void ft_putnbr(int n)
-{
-	if (n > 10)
-		ft_putnbr(n / 10);
-	write(1, &"0123456789"[n % 10], 1);
-}
-
 void print_bits(unsigned char octet)
 {
-	int i = 8;
-	unsigned char bit;
+	int i = 7;
 
 	while (i--)
 	{
-		bit = (octet >> i & 1 + '0');
-		ft_putnbr(bit);
+		if (octet >> i & 1)
+			write(1,"1",1);
+            	else
+                	write(1,"0",1);
 	}
 }
+
 /*
 int main(void)
 {
