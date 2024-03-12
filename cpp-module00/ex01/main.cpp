dd9797d57e56f67ea096d80188e329e0bf422bf6
farukdll <sefoni8668@gmail.com>
2024-03-12 22:40:28 +0300

    cpp-module00 project

---
+++ cpp-module00/ex01/main.cpp
@@ -0,0 +1,28 @@
+#include "PhoneBook.hpp"
+
+int main(void) {
+    std::string     input;
+    PhoneBook       phoneBook(0);
+
+    std::cout << "WELCOME TO YOUR PHONEBOOK" << std::endl;
+    while(1)
+    {
+        std::cout<<"-----------WHAT DO YOU WANT?---------------"<<std::endl;
+        std::cout << "1. ADD\t  : Adds contact to phonephoneBook."<<std::endl;
+        std::cout <<"2. SEARCH : Shows _contacts and searchs."<<std::endl;
+        std::cout <<"3. EXIT   : Closes phonephoneBook."<<std::endl;
+        std::cout <<"---------------------------------------"<<std::endl<<"_>";
+        std::getline(std::cin, input);
+        if (!input.compare("EXIT"))
+            break ;
+        else if (!input.compare("ADD"))
+            phoneBook.addContact();
+        else if (!input.compare("SEARCH"))
+        {
+            phoneBook.displayContacts();
+        }
+        else
+            std::cout <<"INVALID INPUT"<<std::endl;
+    }
+    return (0);
+}
